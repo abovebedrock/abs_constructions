6d59920d4c50076bffb0b0cbabbeae9072724e77
LJM12914 <LJM12914@outlook.com>
2024-08-04 15:53:23 +0800

    1.0.2
    修复使用精准采集附魔物品挖掘非木质竖砖会掉落两个掉落物的 bug

---
+++ abs_vertical_slabs_BP/scripts/blockChange/breakBlock.js
@@ -0,0 +1,37 @@
+﻿//@ts-check
+import { EntityComponentTypes, EntityEquippableComponent, EntityItemComponent, EquipmentSlot, ItemComponentTypes, ItemEnchantableComponent, PlayerBreakBlockBeforeEvent, system } from "@minecraft/server";
+import { isModItem } from "../utils/namespace";
+const wood = ["abs:acacia_slab", "abs:bamboo_mosaic_slab", "abs:bamboo_slab", "abs:birch_slab", "abs:cherry_slab", "abs:crimson_slab", "abs:dark_oak_slab", "abs:jungle_slab", "abs:mangrove_slab", "abs:oak_slab", "abs:spruce_slab", "abs:warped_slab"];
+/**@param {PlayerBreakBlockBeforeEvent} data*/
+export default function breakBlock(data){
+    const id = data.block.typeId;
+    if(isModItem(id) && wood.indexOf(id) == -1){
+        const equipment = /**@type {EntityEquippableComponent | undefined}*/ (data.player.getComponent(EntityComponentTypes.Equippable));
+        if(equipment){
+            const item = equipment.getEquipmentSlot(EquipmentSlot.Mainhand).getItem();
+            if(item){
+                const enchant = /**@type {ItemEnchantableComponent | undefined}*/ (item.getComponent(ItemComponentTypes.Enchantable));
+                if(enchant){
+                    const enchantments = enchant.getEnchantments();
+                    for(let i = 0; i < enchantments.length; i++)if(enchantments[i].type.id == "silk_touch"){
+                        const {location, dimension, typeId, isWaterlogged} = data.block;
+                        system.run(()=>{
+                            const entities = dimension.getEntities({
+                                type: "minecraft:item",
+                                location,
+                                maxDistance: 2
+                            });
+                            for(let i = 0; i < entities.length; i++){
+                                const component = /**@type {EntityItemComponent}*/ (entities[i].getComponent("item"));
+                                if(component.itemStack.typeId == typeId){
+                                    entities[i].remove();
+                                    return;
+                                }
+                            }
+                        });
+                    }
+                }
+            }
+        }
+    }
+}
--- abs_vertical_slabs_BP/scripts/blockChange/index.js
+++ abs_vertical_slabs_BP/scripts/blockChange/index.js
@@ -3,10 +3,13 @@ import { world } from "@minecraft/server";
 import placeWater from "./placeWater";
 import placeBlock from "./placeBlock";
 import collectWater from "./collectWater";
+import breakBlock from "./breakBlock";
+
 
 export default function waterlogInit(){
     world.beforeEvents.itemUseOn.subscribe(placeWater);
     //@ts-ignore 纯粹是扩展更新不过来的事情
     world.beforeEvents.playerPlaceBlock.subscribe(placeBlock);
     //world.beforeEvents.itemUseOn.subscribe(collectWater);
+    world.beforeEvents.playerBreakBlock.subscribe(breakBlock);
 }
